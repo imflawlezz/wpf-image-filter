ef9f2940ca59cc5f625747a51bd19acd214d5da1
imflawlezz <egorart6@gmail.com>
2025-05-03 19:10:27 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_image_filter;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
