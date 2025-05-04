c0dd31a5ccba6590bffda5e2a9d228c2049bb05c
imflawlezz <egorart6@gmail.com>
2025-05-04 17:58:32 +0200

    Added ProcessedImage class

---
+++ ProcessedImage.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace wpf_image_filter;
+
+public class ProcessedImage
+{
+    public string FileName { get; set; } = "";
+    public BitmapImage Thumbnail { get; set; } = new();
+    public string Status { get; set; } = "Ожидание";
+}
